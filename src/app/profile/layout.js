e30d007ee44d7e862936acb886f23493c3319074
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-08 22:09:10 +0545

    layout added  for profile

---
+++ src/app/profile/layout.js
@@ -0,0 +1,10 @@
+export default function ProfileLayout({children}){
+    return (
+        <>
+         <h1> This is profile header</h1> 
+         {children}
+         <h1> This is profile footer</h1> 
+
+        </>
+    )
+}
