b0395b4e3fdd1635d552c5cb2d71a59488b09e55
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-09 21:19:32 +0545

    get post function initialized for users route

---
+++ src/app/api/users/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function GET(request){
+
+    const users = [{
+        name:'kismat bohora',
+        "age":20,
+    }];
+    return NextResponse.json(users);
+}
+
+export function POST(request){
+    return NextResponse.json(req);
+}
