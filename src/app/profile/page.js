fbc25bad14d62486c267a26ac8980f314a4e70ca
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-09 21:20:02 +0545

    profile page customized

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,6 +2,7 @@ export default function Profile(){
     return( <>
      <h1>This is profile page</h1>
      <h1>This page is used by admin and user</h1>
+     <h1>Maintained for both people</h1>
     </>
     )
 }
