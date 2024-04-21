273491406a8f781cb0eb8916ed9b11f9bb594c97
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-21 18:38:48 +0545

    getting single tasks by passing id in url

--- src/app/api/tasks/[taskId]/route.js
+++ src/app/api/tasks/[taskId]/route.js
@@ -1,5 +1,25 @@
-export async function GET(){
+import { getResponseMessage } from "@/helper/responseMessage";
+import { Task } from "@/models/task";
+import { connectDb } from "@/helper/db";
+import { NextResponse } from "next/server";
 
+connectDb();
+
+export async function GET(request, {params}){
+    try{
+        const {taskId} = params;
+        console.log(taskId, 'params id');
+        const tasks = await Task.findById(taskId);
+        console.log(tasks,'tasks user')
+        return NextResponse.json(tasks, {
+            status: 200,
+        });
+        
+    } 
+    catch(err){
+        console.log(err);
+        return getResponseMessage("Error in getting data !!",404, false);
+    }
 }
 
 export async function POST(){
