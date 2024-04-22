0804ce001c56c17d5952260da30460db031b9bf7
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-22 19:11:05 +0545

    info component

---
+++ src/app/info/page.js
@@ -0,0 +1,8 @@
+export default function Info(){
+    return <>
+      <div>
+        <h1>This is info component</h1>
+        <button className="bg-red-600 text-2xl">Click here</button>
+      </div>
+    </>
+}
